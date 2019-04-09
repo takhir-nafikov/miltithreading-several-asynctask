a6e455b438565ed6e610e90c4833dc255c06a929
iandreyshev-home <ivan.andreyshev@gmail.com>
2019-04-09 08:09:03 +0300

    start-point-1-1

--- app/src/main/java/io/github/psgroup/model/CookingState.kt
+++ app/src/main/java/io/github/psgroup/model/CookingState.kt
@@ -2,6 +2,6 @@ package io.github.psgroup.model
 
 sealed class CookingState {
     object NotStarted : CookingState()
-    class InProgress(val max: Int, val current: Int)
-    class Completed(val error: CookingError?)
+    class InProgress(val max: Int, val current: Int) : CookingState()
+    class Completed(val error: CookingError?) : CookingState()
 }
