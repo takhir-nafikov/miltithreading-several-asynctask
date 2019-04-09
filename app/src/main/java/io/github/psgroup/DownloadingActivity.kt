9377b629ce6dc71c936e4f5861d03f1ed61e8a53
iandreyshev-home <ivan.andreyshev@gmail.com>
2019-04-09 06:07:32 +0300

    init commit

---
+++ app/src/main/java/io/github/psgroup/DownloadingActivity.kt
@@ -0,0 +1,13 @@
+package io.github.psgroup
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class DownloadingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_downloading)
+    }
+
+}
