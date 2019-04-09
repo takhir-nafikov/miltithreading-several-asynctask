8c18ba038d5426d34242ae278ade3a8b9dbe68e6
iandreyshev-home <ivan.andreyshev@gmail.com>
2019-04-09 08:09:26 +0300

    start-point-1-1

--- app/src/main/java/io/github/psgroup/presentation/CookActivity.kt
+++ app/src/main/java/io/github/psgroup/presentation/CookActivity.kt
@@ -3,12 +3,36 @@ package io.github.psgroup.presentation
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import io.github.psgroup.R
+import io.github.psgroup.application.PizzaMakerApplication
+import io.github.psgroup.model.CookModel
+import io.github.psgroup.model.CookingState
 
-class CookActivity : AppCompatActivity() {
+class CookActivity : AppCompatActivity(), CookModel.IPresenter {
+
+    private val mModel by lazy { PizzaMakerApplication.cookModel }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cook)
+
+        // TODO: Реализуйте вызовы модели при нажатии на кнопки
+    }
+
+    // TODO: Реализуйте состояния активити
+    override fun update(cookingState: CookingState) = when (cookingState) {
+        CookingState.NotStarted -> Unit
+        is CookingState.InProgress -> Unit
+        is CookingState.Completed -> Unit
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mModel.subscribe(this)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mModel.unsubscribe()
     }
 
 }
